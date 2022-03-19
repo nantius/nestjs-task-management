1d0dd77654f9f91ea0aa79315a1f977ca383583c
Leonardo Spuldar <spdleonardo@hotmail.com>
2022-03-18 21:02:46 -0300

    config settings

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,20 +6,26 @@ import { UsersRepository } from './users.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AuthService, JwtStrategy],
-  controllers: [AuthController],
   imports: [
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: {
-        expiresIn: 3600,
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
     }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
   exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
